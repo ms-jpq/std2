bb9cd28e658b2fe4baa391211212f83f9573acc9
Fly me to the moon <git@bigly.dog>
2020-12-17 19:41:00 -0800

    ugh

--- std2/importlib.py
+++ std2/importlib.py
@@ -1,39 +1,36 @@
 from importlib.abc import Loader
 from importlib.util import module_from_spec, spec_from_file_location
-from os import sep
-from os.path import basename, relpath, splitext
+from itertools import zip_longest
+from os.path import abspath, basename, splitext
 from sys import modules
 from types import ModuleType
 from typing import Iterator, cast
 
+from .os.path import ancestors
+
 
 def _gen_mod_name(path: str, top_level: str) -> str:
     front, _ = splitext(path)
-    base = basename(front)
-    rel_name = relpath(front, start=top_level)
-    if rel_name == base:
-        return f".{rel_name}"
-    else:
 
-        def cont() -> Iterator[str]:
-            prev = ""
-            for c in rel_name:
-                if c == sep:
-                    if prev == ".":
-                        pass
-                    else:
-                        yield "."
-                        prev = "."
-                else:
-                    yield c
-                    prev = c
+    def cont() -> Iterator[str]:
+        for p, t in zip_longest(
+            map(basename, ancestors(front)), map(basename, ancestors(top_level))
+        ):
+            if p == t:
+                pass
+            elif p and not t:
+                yield "."
+                yield p
+            elif t and not p:
+                yield "."
 
-        return "".join(cont())
+    return "".join(cont())
 
 
 def module_from_path(path: str, top_level: str) -> ModuleType:
-    name = _gen_mod_name(path, top_level=top_level)
-    spec = spec_from_file_location(name, path, submodule_search_locations=[])
+    abp = abspath(path)
+    name = _gen_mod_name(abp, top_level=abspath(top_level))
+    spec = spec_from_file_location(name, abp, submodule_search_locations=[])
     mod = module_from_spec(spec)
     modules[mod.__name__] = mod
     cast(Loader, spec.loader).exec_module(mod)
