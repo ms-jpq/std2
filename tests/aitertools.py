44cd086fd9d738a0ebdfce6736cdfc5a1c12f2cc
Fly me to the moon <git@bigly.dog>
2020-12-20 01:19:58 -0800

    more tests

--- tests/aitertools.py
+++ tests/aitertools.py
@@ -1,8 +1,7 @@
 from unittest import IsolatedAsyncioTestCase
 
-
-from ..std2.aitertools import aenumerate, to_async
-from ._consts import MODICUM_REP_FACTOR
+from ..std2.aitertools import aenumerate, atake, to_async
+from ._consts import MODICUM_REP_FACTOR, SMOL_REP_FACTOR
 
 
 class ToAsync(IsolatedAsyncioTestCase):
@@ -20,8 +19,21 @@ class ToAsync(IsolatedAsyncioTestCase):
 
 class AEnum(IsolatedAsyncioTestCase):
     async def test_1(self) -> None:
-        i1 = to_async(enumerate(range(MODICUM_REP_FACTOR)))
-        i2 = aenumerate(to_async(MODICUM_REP_FACTOR))
-        l1 = [i async for i in i1]
-        l2 = [i async for i in i2]
-        self.assertEqual(l2, l1)
+        for j in range(SMOL_REP_FACTOR):
+            i1 = to_async(enumerate(range(MODICUM_REP_FACTOR), start=j))
+            i2 = aenumerate(to_async(range(MODICUM_REP_FACTOR)), start=j)
+            l1 = [i async for i in i1]
+            l2 = [i async for i in i2]
+            self.assertEqual(l2, l1)
+
+
+class ATake(IsolatedAsyncioTestCase):
+    async def test_1(self) -> None:
+        ait = to_async(range(10))
+        l2 = [i async for i in atake(ait, 3)]
+        self.assertEqual(len(l2), 3)
+
+    async def test_1(self) -> None:
+        ait = to_async(range(1))
+        l2 = [i async for i in atake(ait, 3)]
+        self.assertEqual(len(l2), 1)
