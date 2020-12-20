da36f51cc44d19ea65a32b3056f04736b7534bcb
Fly me to the moon <git@bigly.dog>
2020-12-20 01:20:04 -0800

    more tests

--- std2/aitertools.py
+++ std2/aitertools.py
@@ -36,13 +36,13 @@ async def aenumerate(
         yield next(it), item
 
 
-async def chain(*aits: AsyncIterable[T]) -> AsyncIterator[T]:
+async def achain(*aits: AsyncIterable[T]) -> AsyncIterator[T]:
     for ait in aits:
         async for item in ait:
             yield item
 
 
-async def take(ait: AsyncIterable[T], n: int) -> AsyncIterator[T]:
+async def atake(ait: AsyncIterable[T], n: int) -> AsyncIterator[T]:
     if n < 0:
         raise ValueError()
     else:
