7c417d78c506fe4ca5e4c188f39ac71033cb2f03
Fly me to the moon <git@bigly.dog>
2020-12-21 00:36:02 -0800

    std test

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python STD #2
+
+```
+pip3 install -U git@github.com:ms-jpq/std2.git
+```
--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name="std2",
+    version="0.1.0",
+    description="STD #2",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/std2",
+    packages=["std2"],
+)
