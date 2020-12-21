6d743f708b8e3819298e82f481a2c2759052be87
Fly me to the moon <git@bigly.dog>
2020-12-20 19:58:48 -0800

    go

--- std2/asyncio/go.py
+++ std2/asyncio/go.py
@@ -1,4 +1,4 @@
-from asyncio import create_task
+from asyncio import create_task, sleep
 from typing import Awaitable, Callable, TypeVar
 
 T = TypeVar("T")
@@ -7,4 +7,6 @@ GO = Callable[[T], Awaitable[Awaitable[T]]]
 
 
 async def go(aw: Awaitable[T]) -> Awaitable[T]:
-    return create_task(aw)
+    task = create_task(aw)
+    await sleep(0)
+    return task
