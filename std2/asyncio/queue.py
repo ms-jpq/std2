ece817f8b2755ec71d7235477face3da8f1c8671
Fly me to the moon <git@bigly.dog>
2020-12-22 08:38:00 -0800

    typo

--- std2/asyncio/queue.py
+++ std2/asyncio/queue.py
@@ -4,6 +4,6 @@ from typing import AsyncIterator, TypeVar
 T = TypeVar("T")
 
 
-async def to_iter(queue: Queue[T]) -> AsyncIterator:
+async def to_iter(queue: Queue[T]) -> AsyncIterator[T]:
     while True:
         yield await queue.get()
