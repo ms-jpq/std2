c9f062faa8192af71d1af18565d53dbfa6429487
Fly me to the moon <git@bigly.dog>
2020-12-26 19:57:23 -0800

    name change

--- std2/asyncio/_prelude.py
+++ std2/asyncio/_prelude.py
@@ -1,12 +1,10 @@
 from asyncio import get_running_loop
-from asyncio.coroutines import iscoroutine
 from asyncio.futures import Future
-from asyncio.tasks import FIRST_COMPLETED, gather, wait
+from asyncio.tasks import FIRST_COMPLETED, wait
 from functools import partial
 from itertools import chain
-from typing import Any, Awaitable, Callable, Sequence, Tuple, TypeVar
+from typing import Any, Callable, Sequence, Tuple, TypeVar
 
-from .go import GO, go
 
 T = TypeVar("T")
 
@@ -15,13 +13,8 @@ async def pure(item: T) -> T:
     return item
 
 
-async def race(
-    aw: Awaitable[T], *aws: Awaitable[T], go: GO = go
-) -> Tuple[T, Sequence[Future[T]]]:
-    futs = await gather(
-        *(go(a) if iscoroutine(a) else pure(a) for a in chain((aw,), aws))
-    )
-    done, pending = await wait(futs, return_when=FIRST_COMPLETED)
+async def race(aw: Future[T], *aws: Future[T]) -> Tuple[T, Sequence[Future[T]]]:
+    done, pending = await wait(tuple((aw, *aws)), return_when=FIRST_COMPLETED)
     ret = done.pop().result()
     return ret, tuple(chain(done, pending))
 
--- std2/importlib.py
+++ std2/importlib.py
@@ -1,15 +1,14 @@
 from importlib.abc import Loader
 from importlib.util import module_from_spec, spec_from_file_location
-from os import PathLike
 from pathlib import PurePath
 from sys import modules
 from types import ModuleType
-from typing import Iterator, cast
+from typing import Iterator, Union, cast
 
 from .itertools import deiter
 
 
-def _gen_mod_name(path: PathLike, top_level: PathLike) -> str:
+def _gen_mod_name(path: Union[PurePath, str], top_level: Union[PurePath, str]) -> str:
     pp = PurePath(path)
     front_parts = (pp.parent / pp.stem).parts
 
@@ -40,10 +39,15 @@ def _gen_mod_name(path: PathLike, top_level: PathLike) -> str:
     return "".join(cont())
 
 
-def module_from_path(path: PathLike, top_level: PathLike) -> ModuleType:
+def module_from_path(
+    path: Union[PurePath, str], top_level: Union[PurePath, str]
+) -> ModuleType:
     name = _gen_mod_name(path, top_level=top_level)
-    spec = spec_from_file_location(name, path, submodule_search_locations=[])
-    mod = module_from_spec(spec)
-    modules[mod.__name__] = mod
-    cast(Loader, spec.loader).exec_module(mod)
-    return mod
+    if name in modules:
+        raise ImportError()
+    else:
+        spec = spec_from_file_location(name, path, submodule_search_locations=[])
+        mod = module_from_spec(spec)
+        modules[mod.__name__] = mod
+        cast(Loader, spec.loader).exec_module(mod)
+        return mod
