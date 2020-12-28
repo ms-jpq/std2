efe2c05bfbe6c428d7324a7ff9f768eb9b976e3f
Fly me to the moon <git@bigly.dog>
2020-12-27 23:07:50 -0800

    ok

--- tests/pickle.py
+++ tests/pickle.py
@@ -1,17 +1,38 @@
 from unittest import TestCase
 
-from ..std2.pickle import decode
+from ..std2.pickle import CoderError, decode
+from typing import Tuple, Union, Optional
 
 
 class Pickle(TestCase):
     def test_1(self) -> None:
-        n = decode(None, None)
-        self.assertEqual(n, None)
+        thing = decode(None, None)
+        self.assertEqual(thing, None)
 
     def test_2(self) -> None:
-        two = decode(int, 2)
-        self.assertEqual(two, 2)
+        thing = decode(int, 2)
+        self.assertEqual(thing, 2)
 
     def test_3(self) -> None:
-        a = decode(str, "a")
-        self.assertEqual(a, "a")
+        thing = decode(str, "a")
+        self.assertEqual(thing, "a")
+
+    def test_4(self) -> None:
+        thing = decode(Optional[str], "a")
+        self.assertEqual(thing, "a")
+
+    def test_5(self) -> None:
+        thing = decode(Union[int, str], 2)
+        self.assertEqual(thing, 2)
+
+    def test_6(self) -> None:
+        thing = decode(Union[int, str], "a")
+        self.assertEqual(thing, "a")
+
+    def test_7(self) -> None:
+        with self.assertRaises(CoderError):
+            decode(Union[int, str], b"a")
+
+    def test_8(self) -> None:
+        thing = decode(Tuple[int, str], (1, "a"))
+        self.assertEqual(thing, (1, "a"))
