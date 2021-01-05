ae2c539ab028b18b149a189d38a6956e5a6ab895
Fly me to the moon <git@bigly.dog>
2021-01-05 16:00:26 +0700

    freeze test

--- tests/types.py
+++ tests/types.py
@@ -1,5 +1,7 @@
+from typing import Dict, FrozenSet, List, Mapping, Sequence, Set
 from unittest import TestCase
-from ..std2.types import Void, VoidType, or_else
+
+from ..std2.types import Void, VoidType, freeze, or_else
 
 
 class VoidTest(TestCase):
@@ -15,3 +17,14 @@ class VoidTest(TestCase):
     def test_4(self) -> None:
         thing: None = or_else(Void, None)
         self.assertEqual(thing, None)
+
+
+class Freeze(TestCase):
+    def test_1(self) -> None:
+        l1: List[int] = []
+        d1: Dict[int, int] = {}
+        s1: Set[int] = set()
+
+        l2: Sequence[int] = freeze(l1)
+        d2: Mapping[int, int] = freeze(d1)
+        s2: FrozenSet[int] = freeze(s1)
