92355c459c3f904e7f9dbab1bc714d887c497122
Fly me to the moon <git@bigly.dog>
2021-01-05 15:04:52 +0700

    merge!

--- std2/tree.py
+++ std2/tree.py
@@ -1,9 +1,9 @@
 from typing import Any, Mapping, Sequence
 
 
-def merge(ds1: Any, ds2: Any, replace: bool = False) -> Any:
+def _merge(ds1: Any, ds2: Any, replace: bool = False) -> Any:
     if isinstance(ds1, Mapping) and isinstance(ds2, Mapping):
-        append = {k: merge(ds1.get(k), v, replace) for k, v in ds2.items()}
+        append = {k: _merge(ds1.get(k), v, replace) for k, v in ds2.items()}
         return {**ds1, **append}
     if isinstance(ds1, Sequence) and isinstance(ds2, Sequence):
         if replace:
@@ -14,8 +14,8 @@ def merge(ds1: Any, ds2: Any, replace: bool = False) -> Any:
         return ds2
 
 
-def merge_all(ds1: Any, *dss: Any, replace: bool = False) -> Any:
+def merge(ds1: Any, *dss: Any, replace: bool = False) -> Any:
     res = ds1
     for ds2 in dss:
-        res = merge(res, ds2, replace=replace)
+        res = _merge(res, ds2, replace=replace)
     return res
