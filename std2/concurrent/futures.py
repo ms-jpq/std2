35ebbeed9a3a8800d3b8fef1a5fae3f2ef298336
Fly me to the moon <git@bigly.dog>
2021-01-08 12:11:21 +0700

    gathe

--- std2/concurrent/futures.py
+++ std2/concurrent/futures.py
@@ -1,9 +1,9 @@
 from concurrent.futures import ALL_COMPLETED, Future, wait
-from typing import TypeVar
+from typing import TypeVar, cast
 
 T = TypeVar("T")
 
 
 def gather(*futs: Future) -> T:
     wait(futs, return_when=ALL_COMPLETED)
-    return tuple(fut.result() for fut in futs)
+    return cast(T, tuple(fut.result() for fut in futs))
