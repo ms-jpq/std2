a2b5ca4c16543006c07ca7fbb5b2b0b03b8cca15
Fly me to the moon <git@bigly.dog>
2021-01-11 00:44:18 +0700

    ok

--- std2/datetime.py
+++ std2/datetime.py
@@ -1,3 +1,24 @@
 from datetime import datetime, timezone
 
 utcnow = lambda: datetime.now(tz=timezone.utc)
+
+_MINUTE = 60
+_HOUR = 60 * _MINUTE
+_DAY = 24 * _HOUR
+_WEEK = 7 * _DAY
+
+
+def to_minutes(seconds: float) -> float:
+    return seconds / _MINUTE
+
+
+def to_hours(seconds: float) -> float:
+    return seconds / _HOUR
+
+
+def to_days(seconds: float) -> float:
+    return seconds / _DAY
+
+
+def to_weeks(seconds: float) -> float:
+    return seconds / _WEEK
