852b6b43a99e88d49a21045febc9124d724371d3
Fly me to the moon <git@bigly.dog>
2021-01-10 23:29:54 +0700

    decoder

--- std2/pickle/coders.py
+++ std2/pickle/coders.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from inspect import isclass
-from typing import Any, Sequence
+from typing import Any, Sequence, SupportsFloat
 from uuid import UUID
 
 from .decode import DecodeError, Decoders
@@ -17,23 +17,41 @@ def uuid_encoder(thing: Any, encoders: Encoders) -> str:
 def uuid_decoder(
     tp: Any, thing: Any, strict: bool, decoders: Decoders, path: Sequence[Any]
 ) -> UUID:
-    if not isclass(tp) and issubclass(tp, UUID) and isinstance(thing, str):
+    if not (isclass(tp) and issubclass(tp, UUID) and isinstance(thing, str)):
         raise DecodeError(path=tuple((*path, tp)), actual=thing)
     else:
         return UUID(hex=thing)
 
 
-def datetime_encoder(thing: Any, encoders: Encoders) -> str:
+def datetime_str_encoder(thing: Any, encoders: Encoders) -> str:
     if not isinstance(thing, datetime):
         raise EncodeError()
     else:
         return thing.isoformat()
 
 
-def datetime_decoder(
+def datetime_str_decoder(
     tp: Any, thing: Any, strict: bool, decoders: Decoders, path: Sequence[Any]
-) -> UUID:
-    if not isclass(tp) and issubclass(tp, datetime) and isinstance(thing, str):
+) -> datetime:
+    if not (isclass(tp) and issubclass(tp, datetime) and isinstance(thing, str)):
         raise DecodeError(path=tuple((*path, tp)), actual=thing)
     else:
         return datetime.fromisoformat(thing)
+
+
+def datetime_float_encoder(thing: Any, encoders: Encoders) -> float:
+    if not isinstance(thing, datetime):
+        raise EncodeError()
+    else:
+        return thing.timestamp()
+
+
+def datetime_float_decoder(
+    tp: Any, thing: Any, strict: bool, decoders: Decoders, path: Sequence[Any]
+) -> datetime:
+    if not (
+        isclass(tp) and issubclass(tp, datetime) and isinstance(thing, SupportsFloat)
+    ):
+        raise DecodeError(path=tuple((*path, tp)), actual=thing)
+    else:
+        return datetime.fromtimestamp(float(thing), tz=timezone.utc)
