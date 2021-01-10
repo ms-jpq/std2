532975d5d85a2f679a144891255efc3b34d498b6
Fly me to the moon <git@bigly.dog>
2021-01-10 23:13:56 +0700

    coder

--- std2/pickle/coders.py
+++ std2/pickle/coders.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from inspect import isclass
 from typing import Any, Sequence
 from uuid import UUID
@@ -20,3 +21,19 @@ def uuid_decoder(
         raise DecodeError(path=tuple((*path, tp)), actual=thing)
     else:
         return UUID(hex=thing)
+
+
+def datetime_encoder(thing: Any, encoders: Encoders) -> str:
+    if not isinstance(thing, datetime):
+        raise EncodeError()
+    else:
+        return thing.isoformat()
+
+
+def datetime_decoder(
+    tp: Any, thing: Any, strict: bool, decoders: Decoders, path: Sequence[Any]
+) -> UUID:
+    if not isclass(tp) and issubclass(tp, datetime) and isinstance(thing, str):
+        raise DecodeError(path=tuple((*path, tp)), actual=thing)
+    else:
+        return datetime.fromisoformat(thing)
