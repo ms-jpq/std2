edf1bc5904e1fd99d94854e81806df7f5ff5ddd1
Fly me to the moon <git@bigly.dog>
2021-01-10 21:42:45 +0700

    ok

--- std2/contextlib.py
+++ std2/contextlib.py
@@ -1,9 +1,9 @@
 from contextlib import asynccontextmanager
-from typing import AsyncIterator, TypeVar
+from typing import AsyncIterator, Optional, TypeVar, cast
 
 T = TypeVar("T")
 
 
 @asynccontextmanager
-async def nullacontext(enter_result: T = None) -> AsyncIterator[T]:
-    yield enter_result
+async def nullacontext(enter_result: Optional[T] = None) -> AsyncIterator[T]:
+    yield cast(T, enter_result)
