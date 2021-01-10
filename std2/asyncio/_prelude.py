cfcf4ce78c01d0f48d63b5d5678d6353a9685ba7
Fly me to the moon <git@bigly.dog>
2021-01-10 22:24:29 +0700

    use bound

--- std2/asyncio/_prelude.py
+++ std2/asyncio/_prelude.py
@@ -2,15 +2,19 @@ from asyncio import get_running_loop
 from asyncio.futures import Future
 from asyncio.tasks import FIRST_COMPLETED, wait
 from functools import partial
-from typing import Any, Callable, Set, Tuple, TypeVar
+from typing import Any, Callable, FrozenSet, MutableSet, Tuple, TypeVar, cast
+
+from ..types import freeze
 
 T = TypeVar("T")
+_T2 = TypeVar("_T2", bound=Future)
 
 
-async def race(aw: Future, *aws: Future) -> Tuple[Future, Set[Future], Set[Future]]:
-    done, pending = await wait(tuple((aw, *aws)), return_when=FIRST_COMPLETED)
+async def race(aw: _T2, *aws: _T2) -> Tuple[_T2, FrozenSet[_T2], FrozenSet[_T2]]:
+    r: Any = await wait(tuple((aw, *aws)), return_when=FIRST_COMPLETED)
+    done, pending = cast(Tuple[MutableSet[_T2], MutableSet[_T2]], r)
     ret = done.pop()
-    return ret, done, pending
+    return ret, freeze(done), freeze(pending)
 
 
 async def run_in_executor(f: Callable[..., T], *args: Any, **kwargs: Any) -> T:
