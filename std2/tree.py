b1e11c18ff9f50c691a25540983e5eb42399aa06
Fly me to the moon <git@bigly.dog>
2021-01-14 12:58:29 +0700

    added recur sort

--- std2/tree.py
+++ std2/tree.py
@@ -1,4 +1,15 @@
+from collections.abc import ByteString, Iterable, Mapping
+from locale import strxfrm
 from typing import Any, Mapping, Sequence
+
+
+def recur_sort(data: Any) -> Any:
+    if isinstance(data, Mapping):
+        return {k: recur_sort(data[k]) for k in sorted(data, key=strxfrm)}
+    elif isinstance(data, Iterable) and not isinstance(data, (str, ByteString)):
+        return tuple(recur_sort(el) for el in data)
+    else:
+        return data
 
 
 def _merge(ds1: Any, ds2: Any, replace: bool = False) -> Any:
