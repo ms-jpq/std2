b411ddde15c1d980a8b4e934929982d2736a20da
Fly me to the moon <git@bigly.dog>
2021-01-14 14:41:51 +0700

    lib

--- std2/urllib.py
+++ std2/urllib.py
@@ -2,14 +2,8 @@ from http.client import HTTPResponse
 from typing import Union, cast
 from urllib.request import Request, build_opener
 
-from .asyncio import run_in_executor
-
 
 async def urlopen(req: Union[Request, str]) -> HTTPResponse:
-    def fetch() -> HTTPResponse:
-        opener = build_opener()
-        with opener.open(req) as resp:
-            return cast(HTTPResponse, resp)
-
-    msg = await run_in_executor(fetch)
-    return msg
+    opener = build_opener()
+    with opener.open(req) as resp:
+        return cast(HTTPResponse, resp)
