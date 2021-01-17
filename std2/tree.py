186b3ab4b1d6e0aa46480aed4e01dec047fd8334
Fly me to the moon <git@bigly.dog>
2021-01-17 22:36:54 +0700

    tree

--- std2/tree.py
+++ std2/tree.py
@@ -1,6 +1,7 @@
 from collections.abc import Mapping
+from itertools import chain
 from locale import strxfrm
-from typing import Any, Mapping
+from typing import AbstractSet, Any, Mapping
 
 from .types import is_seq
 
@@ -18,7 +19,9 @@ def _merge(ds1: Any, ds2: Any, replace: bool = False) -> Any:
     if isinstance(ds1, Mapping) and isinstance(ds2, Mapping):
         append = {k: _merge(ds1.get(k), v, replace) for k, v in ds2.items()}
         return {**ds1, **append}
-    if is_seq(ds1) and is_seq(ds2):
+    elif isinstance(ds1, AbstractSet) and isinstance(ds2, AbstractSet):
+        return frozenset(chain(ds1, ds2))
+    elif is_seq(ds1) and is_seq(ds2):
         if replace:
             return ds2
         else:
