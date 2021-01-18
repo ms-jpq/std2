6ed054d7549a8f3ba9c6b056ffa4dabdab35e178
Fly me to the moon <git@bigly.dog>
2021-01-18 18:24:10 +0700

    set eq

--- tests/tree.py
+++ tests/tree.py
@@ -7,3 +7,7 @@ class Merge(TestCase):
     def test_1(self) -> None:
         a = merge("a", "b")
         self.assertEqual(a, "b")
+
+    def test_2(self) -> None:
+        a = merge({1}, {2})
+        self.assertEqual(a, {1, 2})
