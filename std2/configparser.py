0757539a74d729b8916bedd27096da37f782462e
Fly me to the moon <git@bigly.dog>
2021-01-18 16:22:58 +0700

    sortkeyby

--- std2/configparser.py
+++ std2/configparser.py
@@ -1,10 +1,14 @@
-from typing import Any, Mapping, MutableMapping, MutableSequence, Sequence, Tuple
+from locale import strxfrm
+from typing import (Any, Iterable, Mapping, MutableMapping, MutableSequence,
+                    Sequence, Tuple)
 
 from .types import is_seq
 
+_Index = Tuple[Sequence[str], Any]
+
 
 def _create_element_at(
-    thing: MutableMapping[str, Any], val: Any, paths: Sequence[str]
+    thing: MutableMapping[str, Any], val: Any, paths: Iterable[str]
 ) -> None:
     if not isinstance(thing, MutableMapping):
         raise ValueError(thing)
@@ -20,13 +24,19 @@ def _create_element_at(
             _create_element_at(thing[head], val=val, paths=tail)
 
 
+def _sort_keyby(index: _Index) -> Any:
+    paths, _ = index
+    return tuple(map(strxfrm, paths))
+
+
 def hydrate(thing: Any) -> Any:
     if isinstance(thing, Mapping):
         thing2: MutableMapping[str, Any] = {}
-        acc: MutableSequence[Tuple[Sequence[str], Any]] = []
+        acc: MutableSequence[_Index] = []
 
         for key, val in thing.items():
             hydrated = hydrate(val)
+
             if isinstance(key, str):
                 paths = key.split(".")
                 if len(paths) > 1:
@@ -36,11 +46,10 @@ def hydrate(thing: Any) -> Any:
             else:
                 thing2[key] = hydrated
 
-        for ps, hydrated in acc:
+        for ps, hydrated in sorted(acc, key=_sort_keyby):
             _create_element_at(thing2, val=hydrated, paths=ps)
 
         return thing2
-
     elif is_seq(thing):
         return tuple(map(hydrate, thing))
     else:
