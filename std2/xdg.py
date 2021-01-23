4f2fd14046d65e6ccec2d016cbda22e15f434aa4
Fly me to the moon <git@bigly.dog>
2021-01-24 03:17:04 +0700

    xdg

---
+++ std2/xdg.py
@@ -0,0 +1,22 @@
+from os import environ
+from pathlib import Path
+
+
+def _xdg_path(env: str, *fallback: str) -> Path:
+    home = environ.get(env)
+    if home:
+        return Path(home)
+    else:
+        return Path(Path.home(), *fallback)
+
+
+def cache_home() -> Path:
+    return _xdg_path("XDG_CACHE_HOME", ".cache")
+
+
+def config_home() -> Path:
+    return _xdg_path("XDG_CONFIG_HOME", ".config")
+
+
+def data_home() -> Path:
+    return _xdg_path("XDG_DATA_HOME", ".local", "share")
