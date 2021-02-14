f4c851830a811c33a9f7f1fa503f08d63e4d748f
Fly me to the moon <git@bigly.dog>
2021-02-13 21:20:02 -0800

    pathlib

--- std2/pathlib.py
+++ std2/pathlib.py
@@ -9,7 +9,7 @@ def walk(path: Path) -> Iterator[Path]:
     for p in path.iterdir():
         if p.is_dir():
             yield from walk(p)
-        elif p.is_file():
+        else:
             yield p
 
 
