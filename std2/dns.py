c65d840aff3984290d39892d66e4615d0f888d5a
Fly me to the moon <git@bigly.dog>
2021-04-11 17:50:12 -0700

    ok

--- std2/dns.py
+++ std2/dns.py
@@ -1,12 +1,12 @@
 from typing import Iterable, Sequence
 
 
-def parts(dns: str, sep=".") -> Sequence[str]:
+def parts(dns: str, sep: str = ".") -> Sequence[str]:
     return dns.split(sep)
 
 
-def parents(dns: str, sep=".") -> Iterable[str]:
+def parents(dns: str, sep: str = ".") -> Iterable[str]:
     _, _, rhs = dns.partition(sep)
     if rhs:
         yield rhs
-        yield from parents(rhs)
+        yield from parents(rhs, sep=sep)
