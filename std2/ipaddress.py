552779c251f910a298ed61f8e31dc0f3ed81b309
Fly me to the moon <git@bigly.dog>
2021-04-17 17:48:07 -0700

    ok

---
+++ std2/ipaddress.py
@@ -0,0 +1,9 @@
+from ipaddress import IPv4Network, IPv6Network
+
+RFC_1918 = (
+    IPv4Network("192.168.0.0/16"),
+    IPv4Network("172.16.0.0/12"),
+    IPv4Network("10.0.0.0/8"),
+)
+
+RFC_4193 = IPv6Network("fd00::/8")
