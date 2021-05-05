9fc5a774d482230b26a8b2ac4c9093d646478751
Fly me to the moon <git@bigly.dog>
2021-05-05 00:52:33 -0700

    rename

--- std2/ipaddress.py
+++ std2/ipaddress.py
@@ -1,12 +1,17 @@
 from ipaddress import IPv4Network, IPv6Network
 
-RFC_1918 = (
-    IPv4Network("192.168.0.0/16"),
-    IPv4Network("172.16.0.0/12"),
+LOOPBACK_V4 = IPv4Network("127.0.0.0/8")
+LOOPBACK_V6 = IPv6Network("::1/128")
+
+PRIVATE_V4 = (
     IPv4Network("10.0.0.0/8"),
+    IPv4Network("172.16.0.0/12"),
+    IPv4Network("192.168.0.0/16"),
 )
 
-RFC_4193 = IPv6Network("fc00::/7")
-RFC_4193_LOCAL = IPv6Network("fd00::/8")
+PRIVATE_V6 = IPv6Network("fc00::/7")
+PRIVATE_V6_ULA = IPv6Network("fd00::/8")
 
-LOCALHOST_V4 = IPv4Network("127.0.0.0/8")
+
+LINK_LOCAL_V4 = IPv4Network("169.254.0.0/16")
+LINK_LOCAL_V6 = IPv6Network("fe80::/10")
