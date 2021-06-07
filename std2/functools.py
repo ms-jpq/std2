a19211da3debfb7f96b472bff43fb89bf146ddd1
Fly me to the moon <git@bigly.dog>
2021-06-07 16:17:31 -0700

    once

--- std2/functools.py
+++ std2/functools.py
@@ -1,9 +1,25 @@
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, TypeVar, cast
 
 from .types import AnyFun
 
 T = TypeVar("T")
 U = TypeVar("U")
+F = TypeVar("F", bound=Callable)
+
+
+def once(f: F) -> F:
+    done, ret = False, None
+
+    def wrapped(*args: Any, **kwargs: Any) -> Any:
+        nonlocal done, ret
+        if done:
+            return ret
+        else:
+            ret = f(*args, **kwargs)
+            done = True
+            return ret
+
+    return cast(F, wrapped)
 
 
 def constantly(thing: T) -> AnyFun[T]:
