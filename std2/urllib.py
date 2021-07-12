c9e8c4be99e6848bcbe4f24404e594dba5fa56e1
DOG DOG DOG! <github@bigly.dog>
2021-07-11 23:32:44 -0700

    opener

--- std2/urllib.py
+++ std2/urllib.py
@@ -2,9 +2,10 @@ from http.client import HTTPResponse
 from typing import Optional, Union, cast
 from urllib.request import Request, build_opener
 
+_OPENER = build_opener()
+
 
 def urlopen(req: Union[Request, str], timeout: Optional[float] = None) -> HTTPResponse:
-    opener = build_opener()
-    resp = opener.open(req, timeout=timeout)
+    resp = _OPENER.open(req, timeout=timeout)
     return cast(HTTPResponse, resp)
 
