e6a741bc38c1f78a56384bc0a290c7172776a07f
DOG DOG DOG! <github@bigly.dog>
2021-07-19 11:33:31 -0700

    prelude

--- std2/asyncio/_prelude.py
+++ std2/asyncio/_prelude.py
@@ -1,11 +1,24 @@
-from asyncio import get_running_loop
+from asyncio import CancelledError, get_running_loop, sleep
 from asyncio.futures import Future
 from asyncio.tasks import FIRST_COMPLETED, wait
+from contextlib import suppress
 from functools import partial
 from typing import AbstractSet, Any, Callable, MutableSet, Tuple, TypeVar, cast
 
 T = TypeVar("T")
 _T2 = TypeVar("_T2", bound=Future)
+
+
+async def cancel(f: Future) -> None:
+    """
+    Cancel task AND wait for task to terminate
+    """
+
+    f.cancel()
+    while not f.done():
+        await sleep(0)
+    with suppress(CancelledError):
+        await f
 
 
 async def race(aw: _T2, *aws: _T2) -> Tuple[_T2, AbstractSet[_T2], AbstractSet[_T2]]:
@@ -19,3 +32,4 @@ async def run_in_executor(f: Callable[..., T], *args: Any, **kwargs: Any) -> T:
     loop = get_running_loop()
     cont = partial(f, *args, **kwargs)
     return await loop.run_in_executor(None, cont)
+
