89107257791faa4d561333806c3a333434f4b9c2
DOG DOG DOG <github@bigly.dog>
2021-07-28 02:55:06 -0700

    tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -32,5 +32,6 @@ def main() -> None:
     runner.run(suite)
 
 
-main()
+if __name__ == "__main__":
+    main()
 
--- tests/_consts.py
+++ tests/_consts.py
@@ -1,6 +1,6 @@
-from os.path import abspath, dirname
+from pathlib import Path
 
-TOP_LEVEL = dirname(dirname(abspath(__file__)))
+TOP_LEVEL = Path(__file__).resolve().parent.parent
 
 SMOL_TIME = 0.1
 MODICUM_TIME = 3
@@ -9,3 +9,4 @@ BIG_TIME = 9
 SMOL_REP_FACTOR = 3
 MODICUM_REP_FACTOR = 30
 BIG_REP_FACTOR = 1000
+
