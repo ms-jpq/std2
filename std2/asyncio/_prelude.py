ba3c38792e9203e559375e112c1c7afe536510a7
Fly me to the moon <git@bigly.dog>
2021-07-30 12:27:22 -0700

    ok

--- std2/asyncio/_prelude.py
+++ std2/asyncio/_prelude.py
@@ -1,6 +1,6 @@
 from asyncio import get_running_loop, sleep
 from asyncio.futures import Future
-from asyncio.tasks import FIRST_EXCEPTION, wait
+from asyncio.tasks import FIRST_COMPLETED, wait
 from functools import partial
 from typing import AbstractSet, Any, Callable, MutableSet, Tuple, TypeVar, cast
 
@@ -19,7 +19,7 @@ async def cancel(f: Future) -> None:
 
 
 async def race(aw: _T2, *aws: _T2) -> Tuple[_T2, AbstractSet[_T2], AbstractSet[_T2]]:
-    r: Any = await wait((aw, *aws), return_when=FIRST_EXCEPTION)
+    r: Any = await wait((aw, *aws), return_when=FIRST_COMPLETED)
     done, pending = cast(Tuple[MutableSet[_T2], MutableSet[_T2]], r)
     ret = done.pop()
     return ret, done, pending
