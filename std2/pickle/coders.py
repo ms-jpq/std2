d5cf765581c7d90db14a223cca53d453d4e0dae7
Fly me to the moon <git@bigly.dog>
2021-07-30 00:42:11 -0700

    q

--- std2/pickle/coders.py
+++ std2/pickle/coders.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from datetime import datetime, timezone
+from email.utils import format_datetime, parsedate_to_datetime
 from ipaddress import (
     IPv4Address,
     IPv4Interface,
@@ -9,10 +11,10 @@ from ipaddress import (
     IPv6Network,
 )
 from pathlib import Path, PurePath
-from typing import Any, Optional, Sequence, Type
+from typing import Any, Optional, Sequence, SupportsFloat, Type
 from uuid import UUID
 
-from .types import DecodeError, Decoder, DParser, DStep, Encoder, EParser
+from .types import DecodeError, Decoder, DParser, DStep, EncodeError, Encoder, EParser
 
 
 def _base_encoder(t: Type) -> Encoder:
@@ -24,12 +26,9 @@ def _base_encoder(t: Type) -> Encoder:
 
             def p(x: Any) -> DStep:
                 if isinstance(x, t):
-                    try:
-                        return True, str(x)
-                    except ValueError as e:
-                        return False, DecodeError(e, path=(*path, tp), actual=x)
+                    return True, str(x)
                 else:
-                    return False, DecodeError(path=(*path, tp), actual=x)
+                    return False, EncodeError(path=(*path, tp), actual=x)
 
             return p
         else:
@@ -82,3 +81,125 @@ DEFAULT_DECODERS = (
     _base_decoder(IPv6Address),
     _base_decoder(IPv4Address),
 )
+
+
+"""
+UNIX
+"""
+
+
+def unix_date_encoder(
+    tp: Any, path: Sequence[Any], encoders: Sequence[Encoder]
+) -> Optional[EParser]:
+    if issubclass(tp, datetime):
+
+        def p(x: Any) -> DStep:
+            if isinstance(x, datetime):
+                return True, x.replace(tzinfo=timezone.utc).timestamp()
+            else:
+                return False, EncodeError(path=(*path, tp), actual=x)
+
+        return p
+    else:
+        return None
+
+
+def unix_date_decoder(
+    tp: Any, path: Sequence[Any], strict: bool, decoders: Sequence[Decoder]
+) -> Optional[DParser]:
+    if not issubclass(tp, datetime):
+        return None
+    else:
+
+        def cont(x: Any) -> DStep:
+            if not isinstance(x, SupportsFloat):
+                return False, DecodeError(path=(*path, tp), actual=x)
+            else:
+                try:
+                    return True, datetime.fromtimestamp(x, tz=timezone.utc)
+                except ValueError as e:
+                    return False, DecodeError(e, path=(*path, tp), actual=x)
+
+        return cont
+
+
+"""
+ISO Date
+"""
+
+
+def iso_date_encoder(
+    tp: Any, path: Sequence[Any], encoders: Sequence[Encoder]
+) -> Optional[EParser]:
+    if issubclass(tp, datetime):
+
+        def p(x: Any) -> DStep:
+            if isinstance(x, datetime):
+                return True, x.replace(tzinfo=timezone.utc).isoformat()
+            else:
+                return False, EncodeError(path=(*path, tp), actual=x)
+
+        return p
+    else:
+        return None
+
+
+def iso_date_decoder(
+    tp: Any, path: Sequence[Any], strict: bool, decoders: Sequence[Decoder]
+) -> Optional[DParser]:
+    if not issubclass(tp, datetime):
+        return None
+    else:
+
+        def cont(x: Any) -> DStep:
+            if not isinstance(x, str):
+                return False, DecodeError(path=(*path, tp), actual=x)
+            else:
+                try:
+                    return True, datetime.fromisoformat(x).replace(tzinfo=timezone.utc)
+                except ValueError as e:
+                    return False, DecodeError(e, path=(*path, tp), actual=x)
+
+        return cont
+
+
+"""
+Internet Date
+"""
+
+
+def internet_date_encoder(
+    tp: Any, path: Sequence[Any], encoders: Sequence[Encoder]
+) -> Optional[EParser]:
+    if issubclass(tp, datetime):
+
+        def p(x: Any) -> DStep:
+            if isinstance(x, datetime):
+                return True, format_datetime(
+                    x.replace(tzinfo=timezone.utc), usegmt=True
+                )
+            else:
+                return False, EncodeError(path=(*path, tp), actual=x)
+
+        return p
+    else:
+        return None
+
+
+def internet_date_decoder(
+    tp: Any, path: Sequence[Any], strict: bool, decoders: Sequence[Decoder]
+) -> Optional[DParser]:
+    if not issubclass(tp, datetime):
+        return None
+    else:
+
+        def cont(x: Any) -> DStep:
+            if not isinstance(x, str):
+                return False, DecodeError(path=(*path, tp), actual=x)
+            else:
+                try:
+                    return True, parsedate_to_datetime(x).replace(tzinfo=timezone.utc)
+                except ValueError as e:
+                    return False, DecodeError(e, path=(*path, tp), actual=x)
+
+        return cont
