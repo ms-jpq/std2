3e1ba19eb192d43e8398a647ea40a293acdc4407
Fly me to the moon <git@bigly.dog>
2021-07-30 01:04:26 -0700

    logging

--- std2/logging.py
+++ std2/logging.py
@@ -1,5 +1,7 @@
 from logging import CRITICAL, DEBUG, ERROR, INFO, NOTSET, WARNING, getLevelName
-from typing import Iterator, Mapping, Tuple
+from os import linesep
+from shutil import get_terminal_size
+from typing import Any, Iterator, Mapping, Tuple
 
 _LEVELS = (CRITICAL, DEBUG, ERROR, INFO, NOTSET, WARNING)
 
@@ -15,3 +17,10 @@ def _gen_lvls() -> Mapping[str, int]:
 
 
 LOG_LEVELS = _gen_lvls()
+
+
+def big_print(thing: Any, sep: str = "-") -> str:
+    col, _ = get_terminal_size()
+    line = sep * col
+    msg = f"{line}{linesep}{thing}{linesep}{line}"
+    return msg
