29943b8c5682ae22f63eb8999b89385c47d82e36
Fly me to the moon <git@bigly.dog>
2021-08-03 12:58:25 -0700

    1 top lv

--- std2/importlib.py
+++ std2/importlib.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 from importlib.abc import Loader
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import PurePath
@@ -8,34 +9,43 @@ from typing import AbstractSet, cast
 from .pathlib import AnyPath
 
 
-def _gen_mod_name(top_levels: AbstractSet[AnyPath], path: AnyPath) -> str:
+def _gen_mod_name(
+    top_level: AnyPath, python_path: AbstractSet[AnyPath], path: AnyPath
+) -> str:
     pp = PurePath(path)
     stem = pp.parent / pp.stem
 
-    for top_level in top_levels:
-        if pp == top_level:
-            raise ValueError()
-        else:
-            try:
-                rel = stem.relative_to(top_level)
-            except ValueError:
-                pass
-            else:
-                return "." + ".".join(rel.parts)
+    if pp in {top_level} | python_path:
+        raise ValueError()
     else:
-        return ".".join(stem.parts)
+        with suppress(ValueError):
+            rel = stem.relative_to(top_level)
+            return "." + ".".join(rel.parts)
+
+        for python_p in python_path:
+            with suppress(ValueError):
+                rel = stem.relative_to(python_p)
+                return ".".join(rel.parts)
+        else:
+            raise ValueError()
 
 
-def module_from_path(top_levels: AbstractSet[AnyPath], path: AnyPath) -> ModuleType:
-    name = _gen_mod_name(top_levels, path=path)
-    if name in modules:
+def module_from_path(
+    top_level: AnyPath, python_path: AbstractSet[AnyPath], path: AnyPath
+) -> ModuleType:
+    try:
+        name = _gen_mod_name(top_level, python_path=python_path, path=path)
+    except ValueError:
         raise ImportError()
     else:
-        spec = spec_from_file_location(name, path, submodule_search_locations=[])
-        if not spec:
+        if name in modules:
             raise ImportError()
         else:
-            mod = module_from_spec(spec)
-            modules[mod.__name__] = mod
-            cast(Loader, spec.loader).exec_module(mod)
-            return mod
+            spec = spec_from_file_location(name, path, submodule_search_locations=[])
+            if not spec:
+                raise ImportError()
+            else:
+                mod = module_from_spec(spec)
+                modules[mod.__name__] = mod
+                cast(Loader, spec.loader).exec_module(mod)
+                return mod
--- tests/importlib.py
+++ tests/importlib.py
@@ -1,61 +1,27 @@
+from os import sep
 from pathlib import PurePath
 from unittest import TestCase
 
 from ..std2.importlib import _gen_mod_name, module_from_path
 from ._consts import TOP_LEVEL
 
+_ROOT = PurePath(sep)
+
 
 class ModFromPath(TestCase):
     def test_1(self) -> None:
-        setup_py = PurePath(TOP_LEVEL, "__init__.py")
+        setup_py = TOP_LEVEL / "__init__.py"
 
-        mod = module_from_path({TOP_LEVEL}, path=setup_py)
+        mod = module_from_path(TOP_LEVEL, python_path=set(), path=setup_py)
         self.assertEqual(mod.__name__, ".__init__")
 
     def test_2(self) -> None:
-        common = PurePath(*"abc")
-        mod = PurePath(common, *"de")
-        top_level = common
-
-        name = _gen_mod_name({top_level}, path=mod)
+        mod = _ROOT / "d" / "e"
+        name = _gen_mod_name(_ROOT, python_path=set(), path=mod)
         self.assertEqual(name, ".d.e")
 
     def test_3(self) -> None:
-        common = PurePath(*"abc")
-        mod = common
-        top_level = PurePath(common, "d")
+        mod = _ROOT / "a" / "b"
 
-        name = _gen_mod_name({top_level}, path=mod)
-        self.assertEqual(name, "a.b.c")
-
-    def test_4(self) -> None:
-        common = PurePath(*"abc")
-        mod = common
-        top_level = PurePath(common, *"de")
-
-        name = _gen_mod_name({top_level}, path=mod)
-        self.assertEqual(name, "a.b.c")
-
-    def test_5(self) -> None:
-        common = PurePath(*"abc")
-        mod = PurePath(common, "d")
-        top_level = PurePath(common, *"ef")
-
-        name = _gen_mod_name({top_level}, path=mod)
-        self.assertEqual(name, "a.b.c.d")
-
-    def test_6(self) -> None:
-        common = PurePath(*"abc")
-        mod = PurePath(common, *"ef")
-        top_level = PurePath(common, "d")
-
-        name = _gen_mod_name({top_level}, path=mod)
-        self.assertEqual(name, "a.b.c.e.f")
-
-    def test_7(self) -> None:
-        common = ""
-        mod = PurePath(common, "a")
-        top_level = PurePath(common, *"de")
-
-        name = _gen_mod_name({top_level}, path=mod)
-        self.assertEqual(name, "a")
+        with self.assertRaises(ValueError):
+            _gen_mod_name(TOP_LEVEL, python_path=set(), path=mod)
