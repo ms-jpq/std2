f57b3691c2cf1c67300b4b60f8dc48a12fdbc95e
Fly me to the moon <git@bigly.dog>
2021-08-05 03:26:25 -0700

    aio

---
+++ std2/asyncio/sys.py
@@ -0,0 +1,36 @@
+from asyncio import (
+    StreamReader,
+    StreamReaderProtocol,
+    StreamWriter,
+    gather,
+    get_running_loop,
+)
+from asyncio.streams import FlowControlMixin
+from dataclasses import dataclass
+from sys import stderr, stdin, stdout
+from typing import IO
+
+
+@dataclass(frozen=True)
+class Aio:
+    stdin: StreamReader
+    stdout: StreamWriter
+    stderr: StreamWriter
+
+
+async def stdio() -> Aio:
+    loop = get_running_loop()
+
+    async def c1() -> StreamReader:
+        sin = StreamReader(loop=loop)
+        await loop.connect_read_pipe(lambda: StreamReaderProtocol(sin), pipe=stdin)
+        return sin
+
+    async def c2(stream: IO) -> StreamWriter:
+        trans, proto = await loop.connect_write_pipe(FlowControlMixin, pipe=stream)
+        w = StreamWriter(transport=trans, protocol=proto, reader=None, loop=loop)
+        return w
+
+    sdin, sdout, sderr = await gather(c1(), c2(stdout), c2(stderr))
+    aio = Aio(stdin=sdin, stdout=sdout, stderr=sderr)
+    return aio
