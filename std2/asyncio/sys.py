4f4fc495cb76f5ba3cf4fda81e049e9cd8b14389
Fly me to the moon <git@bigly.dog>
2021-08-05 05:07:38 -0700

    no devnull

--- std2/asyncio/sys.py
+++ std2/asyncio/sys.py
@@ -7,6 +7,8 @@ from asyncio import (
 )
 from asyncio.streams import FlowControlMixin
 from dataclasses import dataclass
+from os import devnull, fstat
+from os.path import samestat
 from sys import stderr, stdin, stdout
 from typing import IO
 
@@ -19,6 +21,15 @@ class Aio:
 
 
 async def stdio() -> Aio:
+    with open(devnull) as fd:
+        nsd = fstat(fd.fileno())
+        if samestat(fstat(stdin.fileno()), nsd):
+            raise RuntimeError()
+        elif samestat(fstat(stdout.fileno()), nsd):
+            raise RuntimeError()
+        elif samestat(fstat(stderr.fileno()), nsd):
+            raise RuntimeError()
+
     loop = get_running_loop()
 
     async def c1() -> StreamReader:
