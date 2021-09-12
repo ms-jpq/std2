5a6b51217f8c1898af14f6aabdd620ff926cd766
Fly me to the moon <git@bigly.dog>
2021-09-11 22:28:40 -0700

    custom first

--- std2/pickle/decoder.py
+++ std2/pickle/decoder.py
@@ -31,231 +31,233 @@ _T = TypeVar("_T")
 def _new_parser(
     tp: Any, path: Sequence[Any], strict: bool, decoders: Sequence[Decoder]
 ) -> DParser:
-    origin, args = get_origin(tp), get_args(tp)
 
-    if tp is Any:
-        return lambda x: (True, x)
+    for d in decoders:
+        if isclass(tp) and (dp := d(tp, path=path, strict=strict, decoders=decoders)):
+            return dp
+    else:
+        origin, args = get_origin(tp), get_args(tp)
 
-    elif tp is None:
+        if tp is Any:
+            return lambda x: (True, x)
 
-        def p(x: Any) -> DStep:
-            if x is None:
-                return True, None
-            else:
-                return False, DecodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif origin is Literal:
-        a = {*args}
-
-        def p(x: Any) -> DStep:
-            if type(x) in PRIMITIVES and x in a:
-                return True, x
-            else:
-                return False, DecodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif origin is Union:
-        ps = tuple(
-            _new_parser(a, path=path, strict=strict, decoders=decoders) for a in args
-        )
-
-        def p(x: Any) -> DStep:
-            for succ, y in (p(x) for p in ps):
-                if succ:
-                    return True, y
-            else:
-                return False, DecodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif origin in MAPS:
-        lp, rp = (
-            _new_parser(a, path=path, strict=strict, decoders=decoders) for a in args
-        )
-
-        def p(x: Any) -> DStep:
-            if not isinstance(x, Mapping):
-                return False, DecodeError(path=(*path, tp), actual=x)
-            else:
-                acc = {}
-                for k, v in x.items():
-                    sl, l = lp(k)
-                    if not sl:
-                        return False, l
-                    sr, r = rp(v)
-                    if not sr:
-                        return False, r
-
-                    acc[l] = r
-                else:
-                    return True, acc
-
-        return p
-
-    elif origin in SETS:
-        a, *_ = args
-        pp = _new_parser(a, path=path, strict=strict, decoders=decoders)
-
-        def p(x: Any) -> DStep:
-            if not is_iterable_not_str(x):
-                return False, DecodeError(path=(*path, tp), actual=x)
-            else:
-                acc = set()
-                for succ, m in map(pp, x):
-                    if succ:
-                        acc.add(m)
-                    else:
-                        return False, m
-                else:
-                    return True, acc
-
-        return p
-
-    elif origin in SEQS:
-        a, *_ = args
-        pp = _new_parser(a, path=path, strict=strict, decoders=decoders)
-
-        def p(x: Any) -> DStep:
-            if not is_iterable_not_str(x):
-                return False, DecodeError(path=(*path, tp), actual=x)
-            else:
-                acc = []
-                for succ, m in map(pp, x):
-                    if succ:
-                        acc.append(m)
-                    else:
-                        return False, m
-                else:
-                    return True, acc
-
-        return p
-
-    elif origin is tuple:
-        if len(args) >= 2 and args[-1] is Ellipsis:
-            bp = tuple(
-                _new_parser(a, path=path, strict=strict, decoders=decoders)
-                for a in args[:-1]
-            )
-            ep = repeat(
-                _new_parser(args[-2], path=path, strict=strict, decoders=decoders)
-            )
+        elif tp is None:
 
             def p(x: Any) -> DStep:
-                if not is_iterable_not_str(x):
-                    return False, DecodeError(path=(*path, tp), actual=x)
+                if x is None:
+                    return True, None
                 else:
-                    acc = []
-                    for succ, y in (p(m) for p, m in zip(chain(bp, ep), x)):
-                        if succ:
-                            acc.append(y)
-                        else:
-                            return False, y
-                    else:
-                        return True, acc
+                    return False, DecodeError(path=(*path, tp), actual=x)
 
-        else:
+            return p
+
+        elif origin is Literal:
+            a = {*args}
+
+            def p(x: Any) -> DStep:
+                if type(x) in PRIMITIVES and x in a:
+                    return True, x
+                else:
+                    return False, DecodeError(path=(*path, tp), actual=x)
+
+            return p
+
+        elif origin is Union:
             ps = tuple(
                 _new_parser(a, path=path, strict=strict, decoders=decoders)
                 for a in args
             )
 
             def p(x: Any) -> DStep:
+                for succ, y in (p(x) for p in ps):
+                    if succ:
+                        return True, y
+                else:
+                    return False, DecodeError(path=(*path, tp), actual=x)
+
+            return p
+
+        elif origin in MAPS:
+            lp, rp = (
+                _new_parser(a, path=path, strict=strict, decoders=decoders)
+                for a in args
+            )
+
+            def p(x: Any) -> DStep:
+                if not isinstance(x, Mapping):
+                    return False, DecodeError(path=(*path, tp), actual=x)
+                else:
+                    acc = {}
+                    for k, v in x.items():
+                        sl, l = lp(k)
+                        if not sl:
+                            return False, l
+                        sr, r = rp(v)
+                        if not sr:
+                            return False, r
+
+                        acc[l] = r
+                    else:
+                        return True, acc
+
+            return p
+
+        elif origin in SETS:
+            a, *_ = args
+            pp = _new_parser(a, path=path, strict=strict, decoders=decoders)
+
+            def p(x: Any) -> DStep:
+                if not is_iterable_not_str(x):
+                    return False, DecodeError(path=(*path, tp), actual=x)
+                else:
+                    acc = set()
+                    for succ, m in map(pp, x):
+                        if succ:
+                            acc.add(m)
+                        else:
+                            return False, m
+                    else:
+                        return True, acc
+
+            return p
+
+        elif origin in SEQS:
+            a, *_ = args
+            pp = _new_parser(a, path=path, strict=strict, decoders=decoders)
+
+            def p(x: Any) -> DStep:
                 if not is_iterable_not_str(x):
                     return False, DecodeError(path=(*path, tp), actual=x)
                 else:
                     acc = []
-                    for succ, y in (p(m) for p, m in zip(ps, x)):
+                    for succ, m in map(pp, x):
                         if succ:
-                            acc.append(y)
+                            acc.append(m)
                         else:
-                            return False, y
+                            return False, m
                     else:
                         return True, acc
 
-        return p
+            return p
 
-    elif origin and args:
-        raise ValueError(f"Unexpected type -- {tp}")
-
-    elif isclass(tp) and issubclass(tp, Enum):
-
-        def p(x: Any) -> DStep:
-            if not isinstance(x, str):
-                return False, DecodeError(path=(*path, tp), actual=x)
-            else:
-                try:
-                    return True, tp[x]
-                except KeyError:
-                    return False, DecodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif is_dataclass(tp):
-        hints = get_type_hints(tp, globalns=None, localns=None)
-        cls_fields: MutableMapping[str, DParser] = {}
-        rq_fields: MutableSet[str] = set()
-        for field in fields(tp):
-            if field.init:
-                fp = _new_parser(
-                    hints[field.name],
-                    path=(*path, tp, field),
-                    strict=strict,
-                    decoders=decoders,
+        elif origin is tuple:
+            if len(args) >= 2 and args[-1] is Ellipsis:
+                bp = tuple(
+                    _new_parser(a, path=path, strict=strict, decoders=decoders)
+                    for a in args[:-1]
                 )
-                req = field.default is MISSING and field.default_factory is MISSING  # type: ignore
-                cls_fields[field.name] = fp
-                if req:
-                    rq_fields.add(field.name)
+                ep = repeat(
+                    _new_parser(args[-2], path=path, strict=strict, decoders=decoders)
+                )
 
-        def p(x: Any) -> DStep:
-            if not isinstance(x, Mapping):
-                return False, DecodeError(path=(*path, tp), actual=x)
-            else:
-                kwargs: MutableMapping[str, Any] = {}
-                for k, p in cls_fields.items():
-                    if k in x:
-                        succ, v = p(x[k])
-                        if succ:
-                            kwargs[k] = v
+                def p(x: Any) -> DStep:
+                    if not is_iterable_not_str(x):
+                        return False, DecodeError(path=(*path, tp), actual=x)
+                    else:
+                        acc = []
+                        for succ, y in (p(m) for p, m in zip(chain(bp, ep), x)):
+                            if succ:
+                                acc.append(y)
+                            else:
+                                return False, y
                         else:
-                            return False, v
+                            return True, acc
 
-                ks = kwargs.keys()
-                mk = rq_fields - ks
-                if mk:
-                    return False, DecodeError(
-                        path=(*path, tp), actual=x, missing_keys=mk
+            else:
+                ps = tuple(
+                    _new_parser(a, path=path, strict=strict, decoders=decoders)
+                    for a in args
+                )
+
+                def p(x: Any) -> DStep:
+                    if not is_iterable_not_str(x):
+                        return False, DecodeError(path=(*path, tp), actual=x)
+                    else:
+                        acc = []
+                        for succ, y in (p(m) for p, m in zip(ps, x)):
+                            if succ:
+                                acc.append(y)
+                            else:
+                                return False, y
+                        else:
+                            return True, acc
+
+            return p
+
+        elif origin and args:
+            raise ValueError(f"Unexpected type -- {tp}")
+
+        elif isclass(tp) and issubclass(tp, Enum):
+
+            def p(x: Any) -> DStep:
+                if not isinstance(x, str):
+                    return False, DecodeError(path=(*path, tp), actual=x)
+                else:
+                    try:
+                        return True, tp[x]
+                    except KeyError:
+                        return False, DecodeError(path=(*path, tp), actual=x)
+
+            return p
+
+        elif is_dataclass(tp):
+            hints = get_type_hints(tp, globalns=None, localns=None)
+            cls_fields: MutableMapping[str, DParser] = {}
+            rq_fields: MutableSet[str] = set()
+            for field in fields(tp):
+                if field.init:
+                    fp = _new_parser(
+                        hints[field.name],
+                        path=(*path, tp, field),
+                        strict=strict,
+                        decoders=decoders,
                     )
+                    req = field.default is MISSING and field.default_factory is MISSING  # type: ignore
+                    cls_fields[field.name] = fp
+                    if req:
+                        rq_fields.add(field.name)
 
-                if strict:
-                    ek = x.keys() - ks
-                    if ek:
+            def p(x: Any) -> DStep:
+                if not isinstance(x, Mapping):
+                    return False, DecodeError(path=(*path, tp), actual=x)
+                else:
+                    kwargs: MutableMapping[str, Any] = {}
+                    for k, p in cls_fields.items():
+                        if k in x:
+                            succ, v = p(x[k])
+                            if succ:
+                                kwargs[k] = v
+                            else:
+                                return False, v
+
+                    ks = kwargs.keys()
+                    mk = rq_fields - ks
+                    if mk:
                         return False, DecodeError(
-                            path=(*path, tp), actual=x, extra_keys=ek
+                            path=(*path, tp), actual=x, missing_keys=mk
                         )
 
-                return True, tp(**kwargs)
+                    if strict:
+                        ek = x.keys() - ks
+                        if ek:
+                            return False, DecodeError(
+                                path=(*path, tp), actual=x, extra_keys=ek
+                            )
 
-        return p
+                    return True, tp(**kwargs)
 
-    elif tp is float:
+            return p
 
-        def p(x: Any) -> DStep:
-            if isinstance(x, SupportsFloat):
-                return True, x
-            else:
-                return False, DecodeError(path=(*path, tp), actual=x)
+        elif tp is float:
 
-        return p
+            def p(x: Any) -> DStep:
+                if isinstance(x, SupportsFloat):
+                    return True, x
+                else:
+                    return False, DecodeError(path=(*path, tp), actual=x)
 
-    else:
-        for d in decoders:
-            dp = d(tp, path=path, strict=strict, decoders=decoders)
-            if dp:
-                return dp
+            return p
+
         else:
             try:
                 isinstance(None, tp)
--- std2/pickle/encoder.py
+++ std2/pickle/encoder.py
@@ -26,193 +26,194 @@ _T = TypeVar("_T")
 
 
 def _new_parser(tp: Any, path: Sequence[Any], encoders: Sequence[Encoder]) -> EParser:
-    origin, args = get_origin(tp), get_args(tp)
+    for e in encoders:
+        if isclass(tp) and (epp := e(tp, path=path, encoders=encoders)):
+            return epp
+    else:
+        origin, args = get_origin(tp), get_args(tp)
 
-    if tp is Any:
-        return lambda x: (True, x)
+        if tp is Any:
+            return lambda x: (True, x)
 
-    elif tp is None:
-
-        def p(x: Any) -> EStep:
-            if x is None:
-                return True, None
-            else:
-                return False, EncodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif origin is Literal:
-        a = {*args}
-
-        def p(x: Any) -> EStep:
-            if type(x) in PRIMITIVES and x in a:
-                return True, x
-            else:
-                return False, EncodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif origin is Union:
-        ps = tuple(_new_parser(a, path=path, encoders=encoders) for a in args)
-
-        def p(x: Any) -> EStep:
-            for succ, y in (p(x) for p in ps):
-                if succ:
-                    return True, y
-            else:
-                return False, EncodeError(path=(*path, tp), actual=x)
-
-        return p
-
-    elif origin in MAPS:
-        lp, rp = (_new_parser(a, path=path, encoders=encoders) for a in args)
-
-        def p(x: Any) -> EStep:
-            if not isinstance(x, Mapping):
-                return False, EncodeError(path=(*path, tp), actual=x)
-            else:
-                acc = {}
-                for k, v in x.items():
-                    sl, l = lp(k)
-                    if not sl:
-                        return False, l
-                    sr, r = rp(v)
-                    if not sr:
-                        return False, r
-
-                    acc[l] = r
-                else:
-                    return True, acc
-
-        return p
-
-    elif origin in SETS:
-        a, *_ = args
-        pp = _new_parser(a, path=path, encoders=encoders)
-
-        def p(x: Any) -> EStep:
-            if not is_iterable_not_str(x):
-                return False, EncodeError(path=(*path, tp), actual=x)
-            else:
-                acc = {}
-                for succ, m in map(pp, x):
-                    if succ:
-                        acc[m] = True
-                    else:
-                        return False, m
-                else:
-                    return True, acc
-
-        return p
-
-    elif origin in SEQS:
-        a, *_ = args
-        pp = _new_parser(a, path=path, encoders=encoders)
-
-        def p(x: Any) -> EStep:
-            if not is_iterable_not_str(x):
-                return False, EncodeError(path=(*path, tp), actual=x)
-            else:
-                acc = []
-                for succ, m in map(pp, x):
-                    if succ:
-                        acc.append(m)
-                    else:
-                        return False, m
-                else:
-                    return True, acc
-
-        return p
-
-    elif origin is tuple:
-        if len(args) >= 2 and args[-1] is Ellipsis:
-            bp = tuple(_new_parser(a, path=path, encoders=encoders) for a in args[:-1])
-            ep = repeat(_new_parser(args[-2], path=path, encoders=encoders))
+        elif tp is None:
 
             def p(x: Any) -> EStep:
-                if not is_iterable_not_str(x):
-                    return False, EncodeError(path=(*path, tp), actual=x)
+                if x is None:
+                    return True, None
                 else:
-                    acc = []
-                    for succ, y in (p(m) for p, m in zip(chain(bp, ep), x)):
-                        if succ:
-                            acc.append(y)
-                        else:
-                            return False, y
-                    else:
-                        return True, acc
+                    return False, EncodeError(path=(*path, tp), actual=x)
 
-        else:
+            return p
+
+        elif origin is Literal:
+            a = {*args}
+
+            def p(x: Any) -> EStep:
+                if type(x) in PRIMITIVES and x in a:
+                    return True, x
+                else:
+                    return False, EncodeError(path=(*path, tp), actual=x)
+
+            return p
+
+        elif origin is Union:
             ps = tuple(_new_parser(a, path=path, encoders=encoders) for a in args)
 
             def p(x: Any) -> EStep:
+                for succ, y in (p(x) for p in ps):
+                    if succ:
+                        return True, y
+                else:
+                    return False, EncodeError(path=(*path, tp), actual=x)
+
+            return p
+
+        elif origin in MAPS:
+            lp, rp = (_new_parser(a, path=path, encoders=encoders) for a in args)
+
+            def p(x: Any) -> EStep:
+                if not isinstance(x, Mapping):
+                    return False, EncodeError(path=(*path, tp), actual=x)
+                else:
+                    acc = {}
+                    for k, v in x.items():
+                        sl, l = lp(k)
+                        if not sl:
+                            return False, l
+                        sr, r = rp(v)
+                        if not sr:
+                            return False, r
+
+                        acc[l] = r
+                    else:
+                        return True, acc
+
+            return p
+
+        elif origin in SETS:
+            a, *_ = args
+            pp = _new_parser(a, path=path, encoders=encoders)
+
+            def p(x: Any) -> EStep:
+                if not is_iterable_not_str(x):
+                    return False, EncodeError(path=(*path, tp), actual=x)
+                else:
+                    acc = {}
+                    for succ, m in map(pp, x):
+                        if succ:
+                            acc[m] = True
+                        else:
+                            return False, m
+                    else:
+                        return True, acc
+
+            return p
+
+        elif origin in SEQS:
+            a, *_ = args
+            pp = _new_parser(a, path=path, encoders=encoders)
+
+            def p(x: Any) -> EStep:
                 if not is_iterable_not_str(x):
                     return False, EncodeError(path=(*path, tp), actual=x)
                 else:
                     acc = []
-                    for succ, y in (p(m) for p, m in zip(ps, x)):
+                    for succ, m in map(pp, x):
                         if succ:
-                            acc.append(y)
+                            acc.append(m)
                         else:
-                            return False, y
+                            return False, m
                     else:
                         return True, acc
 
-        return p
+            return p
 
-    elif origin and args:
-        raise ValueError(f"Unexpected type -- {tp}")
-
-    elif isclass(tp) and issubclass(tp, Enum):
-
-        def p(x: Any) -> EStep:
-            if not isinstance(x, Enum):
-                return False, EncodeError(path=(*path, tp), actual=x)
-            else:
-                return True, x.name
-
-        return p
-
-    elif is_dataclass(tp):
-        hints = get_type_hints(tp, globalns=None, localns=None)
-        cls_fields: MutableMapping[str, EParser] = {}
-        rq_fields: MutableSet[str] = set()
-        for field in fields(tp):
-            if field.init:
-                fp = _new_parser(
-                    hints[field.name], path=(*path, tp, field), encoders=encoders
+        elif origin is tuple:
+            if len(args) >= 2 and args[-1] is Ellipsis:
+                bp = tuple(
+                    _new_parser(a, path=path, encoders=encoders) for a in args[:-1]
                 )
-                req = field.default is MISSING and field.default_factory is MISSING  # type: ignore
-                cls_fields[field.name] = fp
-                if req:
-                    rq_fields.add(field.name)
+                ep = repeat(_new_parser(args[-2], path=path, encoders=encoders))
 
-        def p(x: Any) -> EStep:
-            if not is_dataclass(x):
-                return False, EncodeError(path=(*path, tp), actual=x)
-            else:
-                acc: MutableMapping[str, Any] = {}
-                for k, p in cls_fields.items():
-                    if hasattr(x, k):
-                        succ, v = p(getattr(x, k))
-                        if succ:
-                            acc[k] = v
+                def p(x: Any) -> EStep:
+                    if not is_iterable_not_str(x):
+                        return False, EncodeError(path=(*path, tp), actual=x)
+                    else:
+                        acc = []
+                        for succ, y in (p(m) for p, m in zip(chain(bp, ep), x)):
+                            if succ:
+                                acc.append(y)
+                            else:
+                                return False, y
                         else:
-                            return False, v
-                    elif req:
-                        return False, EncodeError(
-                            path=(*path, tp), actual=x, missing_keys=k
-                        )
+                            return True, acc
 
-                return True, acc
+            else:
+                ps = tuple(_new_parser(a, path=path, encoders=encoders) for a in args)
 
-        return p
+                def p(x: Any) -> EStep:
+                    if not is_iterable_not_str(x):
+                        return False, EncodeError(path=(*path, tp), actual=x)
+                    else:
+                        acc = []
+                        for succ, y in (p(m) for p, m in zip(ps, x)):
+                            if succ:
+                                acc.append(y)
+                            else:
+                                return False, y
+                        else:
+                            return True, acc
 
-    else:
-        for e in encoders:
-            epp = e(tp, path=path, encoders=encoders)
-            if epp:
-                return epp
+            return p
+
+        elif origin and args:
+            raise ValueError(f"Unexpected type -- {tp}")
+
+        elif isclass(tp) and issubclass(tp, Enum):
+
+            def p(x: Any) -> EStep:
+                if not isinstance(x, Enum):
+                    return False, EncodeError(path=(*path, tp), actual=x)
+                else:
+                    return True, x.name
+
+            return p
+
+        elif is_dataclass(tp):
+            hints = get_type_hints(tp, globalns=None, localns=None)
+            cls_fields: MutableMapping[str, EParser] = {}
+            rq_fields: MutableSet[str] = set()
+            for field in fields(tp):
+                if field.init:
+                    fp = _new_parser(
+                        hints[field.name], path=(*path, tp, field), encoders=encoders
+                    )
+                    req = field.default is MISSING and field.default_factory is MISSING  # type: ignore
+                    cls_fields[field.name] = fp
+                    if req:
+                        rq_fields.add(field.name)
+
+            def p(x: Any) -> EStep:
+                if not is_dataclass(x):
+                    return False, EncodeError(path=(*path, tp), actual=x)
+                else:
+                    acc: MutableMapping[str, Any] = {}
+                    for k, p in cls_fields.items():
+                        if hasattr(x, k):
+                            succ, v = p(getattr(x, k))
+                            if succ:
+                                acc[k] = v
+                            else:
+                                return False, v
+                        elif req:
+                            return False, EncodeError(
+                                path=(*path, tp), actual=x, missing_keys=k
+                            )
+
+                    return True, acc
+
+            return p
+
         else:
             try:
                 isinstance(None, tp)
