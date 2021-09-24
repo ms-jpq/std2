b037d0e63773b7eb2fe4d5c4930098639493905a
Fly me to the moon <git@bigly.dog>
2021-09-24 13:17:21 -0700

    statistics

---
+++ std2/statistics.py
@@ -0,0 +1,14 @@
+from typing import Iterable, Iterator, Mapping, Tuple
+
+
+def quantiles(data: Iterable[float], *quantiles: int) -> Mapping[int, float]:
+    ordered = sorted(data)
+
+    def cont() -> Iterator[Tuple[int, float]]:
+        for quantile in quantiles:
+            assert quantile >= 0 and quantile <= 100
+            if ordered:
+                idx = round((len(ordered) - 1) * quantile)
+                yield quantile, ordered[idx]
+
+    return {k: v for k, v in cont()}
