d88c45320f58a39f930ab6b2f85780df607746b5
IIIIII <github@bigly.dog>
2022-03-27 22:35:08 -0700

    ok

--- std2/asyncio/queue.py
+++ std2/asyncio/queue.py
@@ -1,7 +1,16 @@
+import sys
 from asyncio.queues import Queue
-from typing import Any, AsyncIterator
+from typing import Any, AsyncIterator, TypeVar
 
+if sys.version_info < (3, 9):
 
-async def to_iter(queue: Queue) -> AsyncIterator[Any]:
-    while True:
-        yield await queue.get()
+    async def to_iter(queue: Queue) -> AsyncIterator[Any]:
+        while True:
+            yield await queue.get()
+
+else:
+    _T = TypeVar("_T")
+
+    async def to_iter(queue: Queue[_T]) -> AsyncIterator[_T]:
+        while True:
+            yield await queue.get()
