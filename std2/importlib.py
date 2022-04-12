709927f66df539606baf1032fb1eb2f95535c9f6
IIIIII <github@bigly.dog>
2022-04-12 11:51:10 -0700

    import lib
    ok

--- std2/importlib.py
+++ std2/importlib.py
@@ -1,32 +1,10 @@
-from contextlib import suppress
 from importlib.abc import Loader
 from importlib.util import module_from_spec, spec_from_file_location
+from inspect import getmodulename
 from os.path import normcase
 from pathlib import PurePath
 from sys import modules
 from types import ModuleType
-from typing import AbstractSet
-
-
-def _gen_mod_name(
-    top_level: PurePath, python_path: AbstractSet[PurePath], path: PurePath
-) -> str:
-    pp = PurePath(normcase(path))
-    stem = pp.parent / pp.stem
-
-    if pp in {top_level} | python_path:
-        raise ValueError()
-    else:
-        with suppress(ValueError):
-            rel = stem.relative_to(top_level)
-            return "." + ".".join(rel.parts)
-
-        for python_p in python_path:
-            with suppress(ValueError):
-                rel = stem.relative_to(python_p)
-                return ".".join(rel.parts)
-        else:
-            raise ValueError()
 
 
 def ld_mod(name: str, path: PurePath) -> ModuleType:
@@ -41,18 +19,12 @@ def ld_mod(name: str, path: PurePath) -> ModuleType:
         return mod
 
 
-def module_from_path(
-    top_level: PurePath, python_path: AbstractSet[PurePath], path: PurePath
-) -> ModuleType:
-    try:
-        name = _gen_mod_name(top_level, python_path=python_path, path=path)
-    except ValueError:
-        name = ""
-
-    if not name:
-        raise ImportError()
-    elif name in modules:
-        raise ImportError()
+def ld_mod_from_path(path: PurePath) -> ModuleType:
+    if name := getmodulename(normcase(path)):
+        if name in modules:
+            raise ImportError()
+        else:
+            mod = ld_mod(name, path)
+            return mod
     else:
-        mod = ld_mod(name, path)
-        return mod
+        raise ImportError()
--- tests/importlib.py
+++ tests/importlib.py
@@ -1,31 +1,12 @@
-from os import sep
-from pathlib import PurePath
 from unittest import TestCase
 
-from ..std2.importlib import _gen_mod_name, module_from_path
+from ..std2.importlib import ld_mod_from_path
 from ._consts import TOP_LEVEL
-
-_ROOT = PurePath(sep)
 
 
 class ModFromPath(TestCase):
     def test_1(self) -> None:
         setup_py = TOP_LEVEL / "__init__.py"
 
-        mod = module_from_path(TOP_LEVEL, python_path=set(), path=setup_py)
-        self.assertEqual(mod.__name__, ".__init__")
-
-    def test_2(self) -> None:
-        mod = _ROOT / "d" / "e"
-        name = _gen_mod_name(_ROOT, python_path=set(), path=mod)
-        self.assertEqual(name, ".d.e")
-
-    def test_3(self) -> None:
-        mod = _ROOT / "a" / "b"
-        with self.assertRaises(ValueError):
-            _gen_mod_name(TOP_LEVEL, python_path=set(), path=mod)
-
-    def test_4(self) -> None:
-        mod = _ROOT / "a" / "b"
-        name = _gen_mod_name(TOP_LEVEL, python_path={_ROOT}, path=mod)
-        self.assertEqual(name, "a.b")
+        mod = ld_mod_from_path(setup_py)
+        self.assertEqual(mod.__name__, "__init__")
