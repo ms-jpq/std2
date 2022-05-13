247aa785f280b3ac687d966beeb19c983b9ea13b
IIIIII <github@bigly.dog>
2022-05-13 15:54:33 -0700

    better windows support

--- std2/asyncio/subprocess.py
+++ std2/asyncio/subprocess.py
@@ -1,7 +1,8 @@
 import sys
 from asyncio.subprocess import DEVNULL, PIPE, create_subprocess_exec
 from contextlib import suppress
-from os import environ
+from os import F_OK, environ
+from shutil import which
 from signal import Signals
 from subprocess import CalledProcessError, CompletedProcess
 from typing import AbstractSet, Mapping, Optional
@@ -16,49 +17,54 @@ else:
 
 
 async def call(
-    prog: AnyPath,
-    *args: AnyPath,
+    arg0: AnyPath,
+    *argv: AnyPath,
     kill_signal: Signals = SIGDED,
     capture_stdout: bool = True,
     capture_stderr: bool = True,
     stdin: Optional[bytes] = None,
     cwd: Optional[AnyPath] = None,
     env: Optional[Mapping[str, str]] = None,
-    check_returncode: AbstractSet[int] = frozenset((0,))
+    check_returncode: AbstractSet[int] = frozenset((0,)),
 ) -> _R:
-    proc = await create_subprocess_exec(
-        prog,
-        *args,
-        start_new_session=True,
-        stdin=PIPE if stdin is not None else DEVNULL,
-        stdout=PIPE if capture_stdout else None,
-        stderr=PIPE if capture_stderr else None,
-        cwd=cwd,
-        env=None if env is None else {**environ, **env},
-    )
-    try:
-        cmd = (prog, *args)
-        stdout, stderr = await proc.communicate(stdin)
-        code = await proc.wait()
+    if a0 := which(arg0):
+        proc = await create_subprocess_exec(
+            a0,
+            *argv,
+            start_new_session=True,
+            stdin=PIPE if stdin is not None else DEVNULL,
+            stdout=PIPE if capture_stdout else None,
+            stderr=PIPE if capture_stderr else None,
+            cwd=cwd,
+            env=None if env is None else {**environ, **env},
+        )
+        try:
+            cmd = (arg0, *argv)
+            stdout, stderr = await proc.communicate(stdin)
+            code = await proc.wait()
 
-        if check_returncode and code not in check_returncode:
-            raise CalledProcessError(
-                returncode=code,
-                cmd=cmd,
-                output=stdout if capture_stdout else None,
-                stderr=stderr.decode() if capture_stderr else None,
-            )
-        else:
-            return CompletedProcess(
-                args=cmd,
-                returncode=code,
-                stdout=stdout if capture_stdout else b"",
-                stderr=stderr if capture_stderr else b"",
-            )
-    finally:
-        with suppress(ProcessLookupError):
-            try:
-                kill_children(proc.pid, sig=kill_signal)
-            except PermissionError:
-                proc.kill()
-        await proc.wait()
+            if check_returncode and code not in check_returncode:
+                raise CalledProcessError(
+                    returncode=code,
+                    cmd=cmd,
+                    output=stdout if capture_stdout else None,
+                    stderr=stderr.decode() if capture_stderr else None,
+                )
+            else:
+                return CompletedProcess(
+                    args=cmd,
+                    returncode=code,
+                    stdout=stdout if capture_stdout else b"",
+                    stderr=stderr if capture_stderr else b"",
+                )
+        finally:
+            with suppress(ProcessLookupError):
+                try:
+                    kill_children(proc.pid, sig=kill_signal)
+                except PermissionError:
+                    proc.kill()
+            await proc.wait()
+    elif a0 := which(arg0, mode=F_OK):
+        raise PermissionError(a0)
+    else:
+        raise FileNotFoundError(arg0)
--- std2/subprocess.py
+++ std2/subprocess.py
@@ -1,6 +1,7 @@
 import sys
 from contextlib import suppress
-from os import environ
+from os import F_OK, environ
+from shutil import which
 from signal import Signals
 from subprocess import DEVNULL, PIPE, CalledProcessError, CompletedProcess, Popen
 from typing import AbstractSet, Mapping, Optional
@@ -37,48 +38,53 @@ except ImportError:
 
 
 def call(
-    prog: AnyPath,
-    *args: AnyPath,
+    arg0: AnyPath,
+    *argv: AnyPath,
     kill_signal: Signals = SIGDED,
     capture_stdout: bool = True,
     capture_stderr: bool = True,
     stdin: Optional[bytes] = None,
     cwd: Optional[AnyPath] = None,
     env: Optional[Mapping[str, str]] = None,
-    check: AbstractSet[int] = frozenset((0,))
+    check: AbstractSet[int] = frozenset((0,)),
 ) -> _R:
-    with Popen(
-        (prog, *args),
-        start_new_session=True,
-        stdin=PIPE if stdin is not None else DEVNULL,
-        stdout=PIPE if capture_stdout else None,
-        stderr=PIPE if capture_stderr else None,
-        cwd=cwd,
-        env=None if env is None else {**environ, **env},
-    ) as proc:
-        try:
-            cmd = (prog, *args)
-            stdout, stderr = proc.communicate(stdin)
-            code = proc.wait()
+    if a0 := which(arg0):
+        with Popen(
+            (a0, *argv),
+            start_new_session=True,
+            stdin=PIPE if stdin is not None else DEVNULL,
+            stdout=PIPE if capture_stdout else None,
+            stderr=PIPE if capture_stderr else None,
+            cwd=cwd,
+            env=None if env is None else {**environ, **env},
+        ) as proc:
+            try:
+                cmd = (arg0, *argv)
+                stdout, stderr = proc.communicate(stdin)
+                code = proc.wait()
 
-            if check and code not in check:
-                raise CalledProcessError(
-                    returncode=code,
-                    cmd=cmd,
-                    output=stdout if capture_stdout else None,
-                    stderr=stderr.decode() if capture_stderr else None,
-                )
-            else:
-                return CompletedProcess(
-                    args=cmd,
-                    returncode=code,
-                    stdout=stdout if capture_stdout else b"",
-                    stderr=stderr if capture_stderr else b"",
-                )
-        finally:
-            with suppress(ProcessLookupError):
-                try:
-                    kill_children(proc.pid, sig=kill_signal)
-                except PermissionError:
-                    proc.kill()
-            proc.wait()
+                if check and code not in check:
+                    raise CalledProcessError(
+                        returncode=code,
+                        cmd=cmd,
+                        output=stdout if capture_stdout else None,
+                        stderr=stderr.decode() if capture_stderr else None,
+                    )
+                else:
+                    return CompletedProcess(
+                        args=cmd,
+                        returncode=code,
+                        stdout=stdout if capture_stdout else b"",
+                        stderr=stderr if capture_stderr else b"",
+                    )
+            finally:
+                with suppress(ProcessLookupError):
+                    try:
+                        kill_children(proc.pid, sig=kill_signal)
+                    except PermissionError:
+                        proc.kill()
+                proc.wait()
+    elif a0 := which(arg0, mode=F_OK):
+        raise PermissionError(a0)
+    else:
+        raise FileNotFoundError(arg0)
