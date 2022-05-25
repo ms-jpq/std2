7014b4a5f873284efbc19c61d863eb29c2de4529
IIIIII <github@bigly.dog>
2022-05-25 07:40:41 -0700

    urlpath

--- std2/urllib.py
+++ std2/urllib.py
@@ -1,5 +1,8 @@
 from http.client import HTTPResponse
+from pathlib import PurePosixPath
 from typing import Optional, Union, cast
+from urllib.parse import unquote as _unquote
+from urllib.parse import urlsplit
 from urllib.request import Request, build_opener
 
 _OPENER = build_opener()
@@ -12,3 +15,9 @@ def urlopen(
 ) -> HTTPResponse:
     resp = _OPENER.open(req, data=data, timeout=timeout)
     return cast(HTTPResponse, resp)
+
+
+def uri_path(uri: str, unquote: bool = False) -> PurePosixPath:
+    raw = urlsplit(uri).path
+    path = _unquote(raw) if unquote else raw
+    return PurePosixPath(path)
