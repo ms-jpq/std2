8071350381023f908ef5e1cdfbe40c0e6fdc52dc
IIIIII <git@bigly.dog>
2022-08-11 22:49:17 -0700

    ok

--- std2/asyncio/_prelude.py
+++ std2/asyncio/_prelude.py
@@ -1,5 +1,5 @@
 import sys
-from asyncio import create_task, get_running_loop, sleep
+from asyncio import create_task, gather, get_running_loop, sleep
 from asyncio.futures import Future
 from functools import partial
 from logging import Logger
@@ -22,14 +22,15 @@ def go(log: Logger, aw: Awaitable[_T], suppress: bool = False) -> Awaitable[_T]:
     return create_task(wrapped())
 
 
-def cancel(f: Future) -> Awaitable[None]:
-    f.cancel()
+def cancel(*fs: Future) -> Future:
+    for f in fs:
+        f.cancel()
 
-    async def cont() -> None:
+    async def cont(f: Future) -> None:
         while not f.done():
             await sleep(0)
 
-    return cont()
+    return gather(*map(cont, fs))
 
 
 if sys.version_info < (3, 9):
