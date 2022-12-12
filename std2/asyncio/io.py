1e9b69cd610ce5c6e23dce66314ec4f63631d9a2
root <root@dev.t.home.arpa>
2022-12-12 18:21:49 +0000

    io

--- std2/asyncio/io.py
+++ std2/asyncio/io.py
@@ -1,9 +1,10 @@
 from asyncio import StreamReader, StreamReaderProtocol, StreamWriter, get_running_loop
 from asyncio.streams import FlowControlMixin
-from typing import IO
+from io import BytesIO
+from typing import AsyncIterable, Awaitable, BinaryIO, Tuple
 
 
-async def reader(stream: IO) -> StreamReader:
+async def reader(stream: BinaryIO) -> StreamReader:
     loop = get_running_loop()
     r = StreamReader(loop=loop)
     await loop.connect_read_pipe(
@@ -12,10 +13,20 @@ async def reader(stream: IO) -> StreamReader:
     return r
 
 
-async def writer(stream: IO) -> StreamWriter:
+async def writer(stream: BinaryIO) -> StreamWriter:
     loop = get_running_loop()
     trans, proto = await loop.connect_write_pipe(
         lambda: FlowControlMixin(loop=loop), pipe=stream
     )
     w = StreamWriter(transport=trans, protocol=proto, reader=None, loop=loop)
     return w
+
+
+def io(aiterable: AsyncIterable[bytes]) -> Tuple[BinaryIO, Awaitable[None]]:
+    io = BytesIO()
+
+    async def cont() -> None:
+        async for frame in aiterable:
+            io.write(frame)
+
+    return io, cont()
