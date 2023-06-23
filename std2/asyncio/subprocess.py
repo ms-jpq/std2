e4b2a21e3cd5aa2949c6bb3e81475b1c3c874f0d
IIIIII <git@bigly.dog>
2023-06-23 09:54:32 -0700

    subproc

--- std2/asyncio/subprocess.py
+++ std2/asyncio/subprocess.py
@@ -1,19 +1,47 @@
 import sys
+from asyncio import StreamWriter, gather
 from asyncio.subprocess import DEVNULL, PIPE, create_subprocess_exec
 from contextlib import suppress
 from os import F_OK, environ
 from shutil import which
 from signal import Signals
 from subprocess import CalledProcessError, CompletedProcess
-from typing import IO, AbstractSet, Mapping, Optional, Union
+from typing import (
+    IO,
+    AbstractSet,
+    AsyncIterable,
+    Iterable,
+    Mapping,
+    Optional,
+    Union,
+    cast,
+)
 
 from ..pathlib import AnyPath
 from ..subprocess import SIGDED, kill_children
+from ._prelude import pure
 
 if sys.version_info < (3, 9):
     _R = CompletedProcess
 else:
     _R = CompletedProcess[bytes]
+
+
+async def _write(
+    stdin: StreamWriter, data: Union[bytes, Iterable[bytes], AsyncIterable[bytes]]
+) -> None:
+    try:
+        if isinstance(data, bytes):
+            stdin.write(data)
+        elif isinstance(data, Iterable):
+            for d in data:
+                stdin.write(d)
+        else:
+            async for d in data:
+                stdin.write(d)
+        await stdin.drain()
+    finally:
+        stdin.close()
 
 
 async def call(
@@ -22,28 +50,42 @@ async def call(
     kill_signal: Signals = SIGDED,
     capture_stdout: bool = True,
     capture_stderr: bool = True,
-    stdin: Union[IO[bytes], bytes, None] = None,
+    stdin: Union[None, IO[bytes], bytes, Iterable[bytes], AsyncIterable[bytes]] = None,
     cwd: Optional[AnyPath] = None,
     env: Optional[Mapping[str, str]] = None,
     check_returncode: AbstractSet[int] = frozenset((0,)),
 ) -> _R:
     if a0 := which(arg0):
+        io_in = stdin if isinstance(stdin, IO) else (PIPE if stdin else DEVNULL)
+        io_out = PIPE if capture_stdout else None
+        io_err = PIPE if capture_stderr else None
         proc = await create_subprocess_exec(
             a0,
             *argv,
             start_new_session=True,
-            stdin=PIPE if isinstance(stdin, bytes) else (stdin if stdin else DEVNULL),
-            stdout=PIPE if capture_stdout else None,
-            stderr=PIPE if capture_stderr else None,
+            stdin=io_in,
+            stdout=io_out,
+            stderr=io_err,
             cwd=cwd,
             env=None if env is None else {**environ, **env},
         )
         try:
             cmd = (arg0, *argv)
-            stdout, stderr = await proc.communicate(
-                stdin if isinstance(stdin, bytes) else None
+
+            i = (
+                _write(cast(StreamWriter, proc.stdin), data=cast(bytes, stdin))
+                if io_in == PIPE
+                else pure(None)
             )
-            code = await proc.wait()
+            o = proc.stdout.read() if proc.stdout else pure(b"")
+            e = proc.stderr.read() if proc.stderr else pure(b"")
+
+            code, _, stdout, stderr = await gather(
+                proc.wait(),
+                i,
+                o,
+                e,
+            )
 
             if check_returncode and code not in check_returncode:
                 raise CalledProcessError(
@@ -54,10 +96,7 @@ async def call(
                 )
             else:
                 return CompletedProcess(
-                    args=cmd,
-                    returncode=code,
-                    stdout=stdout if capture_stdout else b"",
-                    stderr=stderr if capture_stderr else b"",
+                    args=cmd, returncode=code, stdout=stdout, stderr=stderr
                 )
         finally:
             with suppress(ProcessLookupError):
