c5fe4f9d44f2da92bc169e0d75bc27a05aecb3ba
IIIIII <github@bigly.dog>
2024-08-25 10:30:54 -0400

    my

--- tests/collections.py
+++ tests/collections.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 from unittest import TestCase
 
 from ..std2.collections import defaultlist
@@ -5,49 +6,49 @@ from ..std2.collections import defaultlist
 
 class DefaultList(TestCase):
     def test_1(self) -> None:
-        l1 = defaultlist(lambda: "")
-        l2 = []
-        ls = [l1, []]
-        for l in ls:
-            self.assertEqual(len(l), 0)
+        l2: Sequence[str] = []
+        ls = [defaultlist(lambda: ""), []]
+        for l1 in ls:
+            self.assertEqual(len(l1), 0)
             with self.assertRaises(IndexError):
-                l[1]
+                l1[1]
             with self.assertRaises(IndexError):
-                l[-1]
-            self.assertEqual(l[:], l2)
+                l1[-1]
+            self.assertEqual(l1[:], l2)
 
     def test_2(self) -> None:
         ls = [defaultlist(lambda: ""), []]
         l2 = ["a"]
-        for l in ls:
-            l.append("a")
-            self.assertEqual(len(l), 1)
+        for l1 in ls:
+            l1.append("a")
+            self.assertEqual(len(l1), 1)
             with self.assertRaises(IndexError):
-                l[1]
+                l1[1]
             with self.assertRaises(IndexError):
-                l[-2]
-            self.assertEqual(l[0], "a")
-            self.assertEqual(l[-1], "a")
-            self.assertEqual(l[:], l2)
+                l1[-2]
+            self.assertEqual(l1[0], "a")
+            self.assertEqual(l1[-1], "a")
+            self.assertEqual(l1[:], l2)
 
     def test_3(self) -> None:
         ls = [defaultlist(lambda: ""), []]
         l2 = ["a"]
-        for l in ls:
-            l.insert(2, "a")
-            self.assertEqual(len(l), 1)
+        for l1 in ls:
+            l1.insert(2, "a")
+            self.assertEqual(len(l1), 1)
             with self.assertRaises(IndexError):
-                l[1]
+                l1[1]
             with self.assertRaises(IndexError):
-                l[-2]
-            self.assertEqual(l[:], l2)
+                l1[-2]
+            self.assertEqual(l1[:], l2)
 
     def test_4(self) -> None:
         ls = [defaultlist(lambda: ""), []]
-        l2 = ["b", "c", "a"]
+        l2 = ["b", "d", "c", "a"]
         for l1 in ls:
             l1.insert(2, "a")
             l1.insert(0, "b")
             l1.insert(-1, "c")
-            self.assertEqual(len(l1), 3)
+            l1.insert(1, "d")
+            self.assertEqual(len(l1), len(l2))
             self.assertEqual(l1[:], l2)
