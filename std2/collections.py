81d69317390fd82a915d85d82b23957103fca747
IIIIII <github@bigly.dog>
2024-08-25 23:01:21 -0400

    give up

--- std2/collections.py
+++ std2/collections.py
@@ -1,14 +1,10 @@
-from collections import defaultdict
-from collections.abc import MutableSequence as MS
-from itertools import chain, count
+from collections.abc import Sequence
 from typing import (
-    Callable,
     Generic,
-    Iterable,
-    MutableMapping,
+    Mapping,
     MutableSequence,
+    Optional,
     SupportsIndex,
-    Tuple,
     TypeVar,
     Union,
     cast,
@@ -18,18 +14,12 @@ from typing import (
 _T = TypeVar("_T")
 
 
-class defaultlist(MS, Generic[_T]):
-    def __init__(self, default_factory: Callable[[], _T]) -> None:
-        self._len = 0
-        self.default_factory = default_factory
-        self._defaultdict: MutableMapping[int, _T] = defaultdict(default_factory)
-        self._debug = False
-
-    def _idx(self, index: SupportsIndex) -> int:
-        i = index.__index__()
-        ai = abs(i)
-        idx = i % self._len if i < 0 and ai <= self._len else ai
-        return idx
+class defaultlist(Sequence, Generic[_T]):
+    def __init__(
+        self, defaultdict: Mapping[int, _T], len: Optional[int] = None
+    ) -> None:
+        self._len = (max(defaultdict) + 1 if defaultdict else 0) if len is None else len
+        self._defaultdict = defaultdict
 
     def __len__(self) -> int:
         return self._len
@@ -44,70 +34,10 @@ class defaultlist(MS, Generic[_T]):
         if isinstance(index, slice):
             return [self._defaultdict[idx] for idx in range(*index.indices(self._len))]
         else:
-            idx = self._idx(index)
+            i = index.__index__()
+            ai = abs(i)
+            idx = i % self._len if i < 0 and ai <= self._len else ai
+
             if idx >= self._len:
                 raise IndexError()
             return cast(MutableSequence[_T], self._defaultdict[idx])
-
-    @overload
-    def __setitem__(self, index: SupportsIndex, value: _T) -> None: ...
-    @overload
-    def __setitem__(self, index: slice, value: Iterable[_T]) -> None: ...
-    def __setitem__(
-        self, index: Union[slice, SupportsIndex], value: Union[Iterable[_T], _T]
-    ) -> None:
-        if isinstance(index, slice):
-            for idx in range(*index.indices(self._len)):
-                self._len = self._len - 1
-                assert self._len >= 0
-                del self._defaultdict[idx]
-
-            re = range(*index.indices(self._len))
-            loop: Iterable[Tuple[int, _T]] = zip(
-                chain(re, count(re.stop)),
-                cast(Iterable[_T], value),
-            )
-        else:
-            idx = self._idx(index)
-            loop = ((idx, cast(_T, value)),)
-
-        for idx, val in loop:
-            self.insert(idx, val)
-
-    def __delitem__(self, index: Union[slice, SupportsIndex]) -> None:
-        if isinstance(index, slice):
-            for idx in range(*index.indices(self._len)):
-                del self._defaultdict[idx]
-        else:
-            idx = self._idx(index)
-            if idx >= self._len:
-                raise IndexError()
-            del self._defaultdict[idx]
-
-    def clear(self) -> None:
-        self._len = 0
-        self._defaultdict.clear()
-
-    def insert(self, index: SupportsIndex, value: _T) -> None:
-        idx = self._idx(index)
-
-        if idx >= self._len:
-            self._defaultdict[self._len] = value
-        else:
-            re = range(self._len, idx, -1)
-            if self._debug:
-                print(f"insert({index}, {value}) | len: {self._len}")
-                print(re, [*re])
-            for i in re:
-                p = i - 1
-                if p in self._defaultdict:
-                    before = dict(self._defaultdict)
-                    self._defaultdict[i] = self._defaultdict[p]
-                    if self._debug:
-                        print(before, f"{p} -> {i}", dict(self._defaultdict))
-            else:
-                self._defaultdict[idx] = value
-                if self._debug:
-                    print(">>", {**self._defaultdict})
-
-        self._len = self._len + 1
--- tests/collections.py
+++ tests/collections.py
@@ -1,143 +1,21 @@
-from dataclasses import dataclass
-from typing import List, MutableSequence, Optional, Sequence, Tuple
+from collections import defaultdict
+from typing import MutableMapping, Sequence, cast
 from unittest import TestCase
 
 from ..std2.collections import defaultlist
 
-_SMS = Sequence[MutableSequence[str]]
-
-_Into = Tuple[Optional[int], Optional[int], Optional[int], Sequence[str]]
-
-
-@dataclass(frozen=True)
-class _TestCase:
-    expected: List[str]
-    into: Sequence[_Into]
-    debug: bool = False
-
 
 class DefaultList(TestCase):
-    def test_1(self) -> None:
-        l2: Sequence[str] = []
-        ls: _SMS = (defaultlist(lambda: ""), [])
-        for l1 in ls:
-            self.assertEqual(len(l1), 0)
-            with self.assertRaises(IndexError):
-                l1[1]
-            with self.assertRaises(IndexError):
-                l1[-1]
-            self.assertEqual(l1[:], l2)
-
-    def test_2(self) -> None:
-        ls: _SMS = (defaultlist(lambda: ""), [])
-        l2 = ["a"]
-        for l1 in ls:
-            l1.append("a")
-            self.assertIn("a", l1)
-            self.assertEqual(len(l1), 1)
-            with self.assertRaises(IndexError):
-                l1[1]
-            with self.assertRaises(IndexError):
-                l1[-2]
-            self.assertEqual(l1[0], "a")
-            self.assertEqual(l1[-1], "a")
-            self.assertEqual(l1[:], l2)
-
-    def test_3(self) -> None:
-        ls: _SMS = (defaultlist(lambda: ""), [])
-        l2 = ["a"]
-        for l1 in ls:
-            l1.insert(2, "a")
-            self.assertEqual(len(l1), 1)
-            with self.assertRaises(IndexError):
-                l1[1]
-            with self.assertRaises(IndexError):
-                l1[-2]
-            self.assertEqual(l1[:], l2)
-
-    def test_4(self) -> None:
-        ls: _SMS = (defaultlist(lambda: ""), [])
-        l2 = ["b", "d", "c", "a"]
-        for l1 in ls:
-            l1.insert(2, "a")
-            l1.insert(0, "b")
-            l1.insert(-1, "c")
-            l1.insert(1, "d")
-            self.assertEqual(len(l1), len(l2))
-            self.assertEqual(l1[:], l2)
-
     def test_5(self) -> None:
-        for case in (
-            _TestCase(into=((None, None, None, ()),), expected=[]),
-            _TestCase(
-                into=((None, None, None, ("a", "b", "c")),),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("a", "b", "c")),
-                    (None, None, None, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("e", "f")),
-                    (None, None, None, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("e", "f", "g", "h")),
-                    (None, None, None, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("e", "f", "g", "h")),
-                    (0, 4, None, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("e", "f", "g", "h")),
-                    (0, 4, 1, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("e", "f", "g", "h")),
-                    (0, 99, None, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            _TestCase(
-                into=(
-                    (None, None, None, ("e", "f", "g", "h")),
-                    (0, 99, 1, ("a", "b", "c")),
-                ),
-                expected=["a", "b", "c"],
-            ),
-            # _TestCase(
-            #     into=(
-            #         (None, None, None, ("e", "f", "g", "h", "i")),
-            #         (0, 2, None, ("a", "b", "c")),
-            #     ),
-            #     expected=["a", "b", "c", "g", "h", "i"],
-            #     debug=True,
-            # ),
+        for d0, l2 in (
+            ({}, cast(Sequence[str], [])),
+            ({0: "a"}, ["a"]),
+            ({1: "a"}, ["", "a"]),
+            ({2: "a"}, ["", "", "a"]),
+            ({2: "a", 4: "b"}, ["", "", "a", "", "b"]),
         ):
-            l1 = defaultlist(lambda: "")
-            if case.debug:
-                l1._debug = True
-            l2: MutableSequence[str] = []
-            for i, j, k, into in case.into:
-                l2[i:j:k] = into
-                l1[i:j:k] = into
+            d1: MutableMapping[int, str] = defaultdict(lambda: "")
+            d1.update(d0)
+            l1 = defaultlist(d1)
 
-            self.assertEqual(l2[:], case.expected, case)
-            self.assertEqual(l1[:], case.expected, case)
+            self.assertEqual(l1[:], l2)
